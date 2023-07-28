d8cc1bca0b6a8658aab08116e4ec5c9776cbc669
Eric Biao <eeric2779@gmail.com>
2023-07-27 22:39:17 -0300

    Projeto criado

---
+++ src/test/java/com/ericzCorp/revenda/RevendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericzCorp.revenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
